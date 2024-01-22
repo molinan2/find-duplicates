c3ef509ac1d889573e79753365d3f0c34e51b2a8
Juanma <molinan2@gmail.com>
2024-01-22 16:52:03 +0100

    Criteria

--- README.md
+++ README.md
@@ -15,14 +15,22 @@ The current folder and all its subfolders will be traversed recursively. Results
 * `items.json`: The complete list of files.
 * `duplicates.json`: The duplicated files.
 
-
 ### Folder
 
-Specify a custom folder by adding the `-p` (`--path`) option:
+Default is `.`. Specify a custom folder by adding the `-p` (`--path`) option:
 
 ```shell
 node index.js -p "Your folder"
 node index.js --path "Your folder"
 ```
 
-Default folder is `.`.
+### Criteria
+
+Default is `hash`. Criteria can be `hash` (slow) or `size` (fast but inaccurate). Specify a criteria by adding the `-c` (`--criteria`) option:
+
+```shell
+node index.js -c "hash"
+node index.js -c "size"
+node index.js --criteria "hash"
+node index.js --criteria "size"
+```
--- index.js
+++ index.js
@@ -2,12 +2,24 @@ import fs from 'fs';
 import minimist from 'minimist';
 import { walk } from './lib/walker.js';
 
+const CRITERIA_HASH = 'hash';
+const CRITERIA_SIZE = 'size';
+
 const path = parsePath();
-const items = walk(path, { hash: false });
+const options = (() => {
+    const criteria = parseCriteria();
+    switch (criteria) {
+        case CRITERIA_HASH: return { hash: true };
+        case CRITERIA_SIZE: return { hash: false };
+        default: return { hash: true };
+    }
+})();
+
+const items = walk(path, options);
 console.log('Found', items.length, 'items');
 fs.writeFileSync('./results/items.json', JSON.stringify(items, null, 4));
 
-const duplicates = findDuplicates(items, { hash: false });
+const duplicates = findDuplicates(items, options);
 console.log('Found', duplicates.length, 'duplicates');
 fs.writeFileSync('./results/duplicates.json', JSON.stringify(duplicates, null, 4));
 
@@ -25,13 +37,28 @@ function parsePath() {
 
 /**
  *
+ * @return {*|string}
+ */
+function parseCriteria() {
+    const DEFAULT_CRITERIA = CRITERIA_HASH;
+    const args = minimist(process.argv);
+    const criteria = args['c'] || args['criteria'] || DEFAULT_CRITERIA;
+
+    if (![CRITERIA_HASH, CRITERIA_SIZE].includes(criteria)) {
+        return DEFAULT_CRITERIA;
+    }
+
+    return criteria;
+}
+
+/**
+ *
  * @param items
  * @param options
  * @return {*[]}
  */
 function findDuplicates(items, options) {
     const duplicates = [];
-
 
     for (const item of items) {
         const filtered = items.filter(e => {
